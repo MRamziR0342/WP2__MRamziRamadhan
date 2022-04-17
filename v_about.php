31092cdd7101d2cf2494eb9a7c15355660ab666b
MRamziR0342 <101614705+MRamziR0342@users.noreply.github.com>
2022-04-17 14:41:13 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+    <li>Nama Depan : Ramzi</li>
+    <li>Nama Belakang : Ramadhan</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+    <li> Jalan Tanah Baru No. 4, Depok</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+    <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+    <li>Main Game</li>
+    <li>Lari</li>
+ </ul>
+</section>
